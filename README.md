c3bdf90def3246d0be076bb69b536d3362b4ff67
Han <tinghan0110@gmail.com>
2023-01-22 22:48:36 +0800

    Chore (#6)
    * doc: fix `README.md` title
    
    * chore: simplify benchmark `zero_check`

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# HyerPlonk
+# HyperPlonk
 
 ## Benchmark
 
--- benches/zero_check.rs
+++ benches/zero_check.rs
@@ -2,7 +2,7 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use halo2_curves::bn256::{Fr, G1Affine};
 use hyperplonk::{
     piop::sum_check::{
-        vanilla::{EvaluationsProver, VanillaSumCheck, VanillaSumCheckProver},
+        vanilla::{EvaluationsProver, VanillaSumCheck},
         SumCheck, VirtualPolynomial,
     },
     snark::hyperplonk::util::{plonk_expression, rand_plonk_assignment},
@@ -12,9 +12,11 @@ use hyperplonk::{
     },
 };
 
-fn run<P: VanillaSumCheckProver<Fr>>(num_vars: usize, virtual_poly: VirtualPolynomial<Fr>) {
+type ZeroCheck = VanillaSumCheck<EvaluationsProver<Fr, true>>;
+
+fn run(num_vars: usize, virtual_poly: VirtualPolynomial<Fr>) {
     let mut transcript = Keccak256Transcript::<_, G1Affine>::new(Vec::new());
-    VanillaSumCheck::<P>::prove(&(), num_vars, virtual_poly, Fr::zero(), &mut transcript).unwrap();
+    ZeroCheck::prove(&(), num_vars, virtual_poly, Fr::zero(), &mut transcript).unwrap();
 }
 
 fn zero_check(c: &mut Criterion) {
@@ -30,14 +32,10 @@ fn zero_check(c: &mut Criterion) {
     group.sample_size(10);
     for num_vars in 20..26 {
         let (expression, polys, challenges, ys) = setup(num_vars);
+        let virtual_poly = VirtualPolynomial::new(&expression, &polys, &challenges, &ys);
         let id = BenchmarkId::from_parameter(num_vars);
         group.bench_with_input(id, &num_vars, |b, &num_vars| {
-            b.iter(|| {
-                run::<EvaluationsProver<Fr, true>>(
-                    num_vars,
-                    VirtualPolynomial::new(&expression, &polys, &challenges, &ys),
-                )
-            });
+            b.iter(|| run(num_vars, virtual_poly.clone()));
         });
     }
 }
